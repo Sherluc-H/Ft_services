4ecfd7ac9c3d8fb80c21ef61b2793dbd9d768874
Luc HUANG <lhuang@e3r5p6.42.fr>
2020-01-29 15:48:57 +0100

    change mysql service

--- srcs/mysql/build_image.sh
+++ srcs/mysql/build_image.sh
@@ -1,3 +1,6 @@
+echo "use wordpress;" > ./srcs/change_ip
+echo "update wp_options set option_value = \"http://$(minikube ip)/wordpress\" where option_id = 1;" >> ./srcs/change_ip
+echo "update wp_options set option_value = \"http://$(minikube ip)/wordpress\" where option_id = 2;" >> ./srcs/change_ip
 eval $(minikube docker-env)
 docker build . -t mysql
 kubectl apply -f mysql_deploy.yaml
--- srcs/mysql/srcs/run.sh
+++ srcs/mysql/srcs/run.sh
@@ -1,2 +1,9 @@
-mysql -u root < mysql_admin
-#mysql -u root wordpress < wordpress.sql
+#! /bin/sh
+if [[ ! -d /var/lib/mysql/wordpress ]]
+then
+	mysql_install_db --user=mysql --datadir=${DB_DATA_PATH}
+	rc-service mariadb start
+	mysql -u root < mysql_admin
+	mysql -u root wordpress < wordpress.sql
+	mysql -u root < change_ip
+fi
